bccddbec5d120b800a57ac8d6be44aeecb4cf417
Thawkster <tristan@nithon.com>
2019-09-11 14:51:48 -0700

    [FIX] Gets api caught in a loop if nothing returned

--- app.py
+++ app.py
@@ -10,19 +10,24 @@ app = Flask(__name__)
 def index():
 
     search_bar = request.args.get('search_bar')
-    if request.args.get('random') == "random" or request.args.get('search_bar') == "":
-        f=open("static/random.txt", "r")
-        word_list = f.readlines()
-        f.close
-        word_list = word_list[0].split(' ')
-        search_bar = choice(word_list)
-    params = {
-        "q": search_bar,
-        "key": "B76YW88VZ3MZ"
-    }
-
-    response = requests.get("https://api.tenor.com/v1/search", params=params).json()
-    gifs = response["results"][0:10]
+    status = False
+    while status == False:
+        if request.args.get('random') == "random" or request.args.get('search_bar') == "":
+            f=open("static/random.txt", "r")
+            word_list = f.readlines()
+            f.close
+            word_list = word_list[0].split(' ')
+            search_bar = choice(word_list)
+        params = {
+            "q": search_bar,
+            "key": "B76YW88VZ3MZ"
+        }
+        response = requests.get("https://api.tenor.com/v1/search", params=params).json()
+        
+            
+        gifs = response["results"][0:10]
+        if gifs:
+            status = True
 
     """Return homepage."""
     return render_template("index.html", response=response, gifs=gifs, search_bar= search_bar)
