84139e8a75c52c2bd1987ffb65a6c59c1952cf26
sprajjwal <sprajjwal@ucdavis.edu>
2019-09-12 15:59:31 -0700

    [ADD]added trending button

--- app.py
+++ app.py
@@ -8,23 +8,29 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     search_bar = request.args.get('search_bar')
-    status = True
-    while status:
-        if request.args.get('random') == "random" or request.args.get('search_bar') == "" or request.args.get('search_bar') == None:
-            f=open("static/random.txt", "r")
-            word_list = f.readlines()
-            f.close
-            word_list = word_list[0].split(' ')
-            search_bar = choice(word_list)
-        params = {
-            "q": search_bar,
-            "key": "B76YW88VZ3MZ"
-        }
-        response = requests.get("https://api.tenor.com/v1/search", params=params).json()    
-        gifs = response["results"][0:10]
-        if gifs or request.args.get('search_bar'):
-            status = False
+    if request.args.get('trending') == 'trending':
+        print("in trending")
+        response = requests.get("https://api.tenor.com/v1/trending").json()
+        search_bar = "Trending"
+    else:
+        status = True
+        while status:
+            if request.args.get('random') == "random" or request.args.get('search_bar') == "" or request.args.get('search_bar') == None:
+                f=open("static/random.txt", "r")
+                word_list = f.readlines()
+                f.close
+                word_list = word_list[0].split(' ')
+                search_bar = choice(word_list)
+            params = {
+                "q": search_bar,
+                "key": "B76YW88VZ3MZ"
+            }
+            response = requests.get("https://api.tenor.com/v1/search", params=params).json() 
+            gifs = response["results"][0:10]   
+            if gifs or request.args.get('search_bar'):
+                status = False
 
+    gifs = response["results"][0:10]
     """Return homepage."""
     return render_template("index.html", response=response, gifs=gifs, search_bar= search_bar)
 
