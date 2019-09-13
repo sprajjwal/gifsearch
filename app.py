d3d739f68826fa6eef89a9316170c4605713d8f7
sprajjwal <sprajjwal@ucdavis.edu>
2019-09-13 11:21:18 -0700

    [ADD]added comments

--- app.py
+++ app.py
@@ -3,30 +3,33 @@ from random import sample, choice
 import requests 
 import json
 
+# flask instantiation
 app = Flask(__name__)
 
+# route to the homepage
 @app.route('/')
 def index():
-    search_bar = request.args.get('search_bar')
-    if request.args.get('trending') == 'trending':
+    search_bar = request.args.get('search_bar') #query for search
+    if request.args.get('trending') == 'trending':  # trending gifs
         print("in trending")
         response = requests.get("https://api.tenor.com/v1/trending").json()
         search_bar = "Trending"
     else:
         status = True
-        while status:
+        while status:  # loop to get random gifs so we don't get a page without gifs
+            # grabbing a word from random.txt from spaceman
             if request.args.get('random') == "random" or request.args.get('search_bar') == "" or request.args.get('search_bar') == None:
                 f=open("static/random.txt", "r")
                 word_list = f.readlines()
                 f.close
                 word_list = word_list[0].split(' ')
                 search_bar = choice(word_list)
-            params = {
+            params = {  # parameters for API request
                 "q": search_bar,
                 "key": "B76YW88VZ3MZ"
             }
-            response = requests.get("https://api.tenor.com/v1/search", params=params).json() 
-            gifs = response["results"][0:10]   
+            response = requests.get("https://api.tenor.com/v1/search", params=params).json() # API request
+            gifs = response["results"][0:10]   # Processing API for individual gifs
             if gifs or request.args.get('search_bar'):
                 status = False
 
